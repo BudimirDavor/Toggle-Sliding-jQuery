0c698d5e658fc79f1f512e01ccfccf9703214c30
Davor Budimir <47507294+BudimirDavor@users.noreply.github.com>
2021-04-16 17:09:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+$(function() 
+{
+    $("#question1").click(function() {
+        $("#answer1").slideToggle(500);
+    });
+	$("#question2").click(function() {
+        $("#answer2").slideToggle(500);
+    });
+	$("#question3").click(function() {
+        $("#answer3").slideToggle(500);
+    });
+	$("#question4").click(function() {
+        $("#answer4").slideToggle(500);
+    });
+	$("#question5").click(function() {
+        $("#answer5").slideToggle(500);
+    });
+	$("#question6").click(function() {
+        $("#answer6").slideToggle(500);
+    });
+});
